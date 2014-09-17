2740b7e0d71e3d6542c766bd45369b7db89a4507
wsbjwjt <wsbjwjt@foxmail.com>
2014-09-17 20:31:04 +0800

    Add code
    additional codes

--- Classes/ChoiceScene.cpp
+++ Classes/ChoiceScene.cpp
@@ -29,7 +29,7 @@ bool ChoiceScene::init() {
     pSprite->setScaleY(winSize.height / szSprite.height);
     this->addChild(pSprite, 0);
     
-    const char *szImgs[4] = {"paddle/btn_play.png", "paddle/btn_rewind.png", "paddle/btn_fast_forward.png", "fonts/fps_imgaes.png"};
+    const char *szImgs[4] = {"paddle/btn_play.png", "paddle/btn_rewind.png", "paddle/btn_fast_forward.png", "fonts/fps_images.png"};
     float fSettings[4][4] = {
         {20.0f / 480, 16.0f / 320, 280.0f / 480, 160.0f / 320},
         {20.0f / 480, 16.0f / 320, 200.0f / 480, 160.0f / 320},
@@ -37,15 +37,31 @@ bool ChoiceScene::init() {
         {20.0f / 480, 22.0f / 320, 280.0f / 480, 172.0f / 320}
     };
     
-    for (int i = 0; i < 3; i ++) {
+    for (int i = 0; i < 3; ++i) {
         
         float* fSetting = fSettings[i];
-        Texture2D* paddleTexture = TextureCache::getInstance()->addImage(szImgs[i]);
-        
+        Texture2D* paddleTexture = Director::getInstance()->getTextureCache()->addImage("Black.png");
+        Sprite* pPaddle = Sprite::createWithTexture(paddleTexture);
+        Size szBtn = pPaddle->getContentSize();
+        pPaddle->setScaleX(winSize.width / szBtn.width * fSetting[0]);
+        pPaddle->setScaleY(winSize.height / szBtn.height * fSetting[1]);
+        pPaddle->setPosition(Vec2(winSize.width * fSetting[2], winSize.height * fSetting[3]));
+        this->addChild(pPaddle);
         
     }
     
+    int i = 3;
+    Color3B color = {0, 0, 0};
+    float* fSetting = fSettings[i];
+    LabelAtlas* label1 = LabelAtlas::create("1", szImgs[i], 16, 32, '.');
     
+    Size sz = label1->getContentSize();
+    label1->setAnchorPoint(Vec2(0.5f, 0.5f));
+    label1->setScaleX(winSize.width / sz.width * fSetting[0]);
+    label1->setScaleY(winSize.height / sz.height * fSetting[1]);
+    label1->setPosition(Vec2(winSize.width * fSetting[2], winSize.height * fSetting[3]));
+    label1->setColor(color);
+    this->addChild(label1, 0);
     
     
     return true;
